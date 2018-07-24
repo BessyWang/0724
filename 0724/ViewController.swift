889fe82825c257c21669d426d8ac9b3a05a45318
王沛翎 <a0983455127@icloud.com>
2018-07-24 22:53:28 +0800

    Initial Commit

---
+++ 0724/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  0724
+//
+//  Created by Bessy Wang on 2018/7/24.
+//  Copyright © 2018年 Bessy Wang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
